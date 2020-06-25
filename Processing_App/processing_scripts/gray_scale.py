a4b8feb9814afed54fc9d2b2717e9b9e7ef52603
Derrick <dykderrick@outlook.com>
2020-06-26 02:56:08 +0800

    Update to cater to report. Add docString.

--- Processing_App/processing_scripts/gray_scale.py
+++ Processing_App/processing_scripts/gray_scale.py
@@ -10,24 +10,63 @@ import numpy as np
 from Processing_App.processing_scripts.image import Image
 
 
+def _convert2gray(raw_image):
+    """
+    Use numpy to calc grayscale image.
+    :param raw_image: r, g, b channels ndarray of image
+    :return: general gray level image
+    """
+    # get r, g, b channels
+    r, g, b = raw_image[:, :, 0], raw_image[:, :, 1], raw_image[:, :, 2]
+
+    # calc general gray level
+    # See reference: https://en.wikipedia.org/wiki/Grayscale#Converting_color_to_grayscale
+    return 0.2989 * r + 0.5870 * g + 0.1140 * b
+
+
 class GrayImage(Image):
+    """
+    This class defines a process of making image into a specific gray scale level.
+    For example, gray_scale = 128 will make the image become a gray image with every pixel value is between 0 and 128.
+    """
+
     def __init__(self, image_path, gray_scale):
+        """
+        Constructor.
+        :param image_path: absolute path of image to be processed.
+        :param gray_scale: int
+        """
         super().__init__(image_path, str(gray_scale) + "GRAYED")
         self._gray_scale = int(gray_scale)
 
         self._process()
 
-    def _process(self):
-        img = cv2.imread(self._image_path)
-        h, w, c = img.shape
-
-        if c != 3:
-            raise Exception("ALREADY GRAYED")
-
-        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # TODO
+    def _convert2gray_scale(self, gray_img):
+        """
+        Convert a gray image into a specific gray scale.
+        Just remapping.
+        :param gray_img: single channel of image in a type of ndarray.
+        :return: scaled_pixels ndarray.
+        """
         scaled_pixels = []
+
         for index, row_pixels in enumerate(gray_img):
+            # restrict gray range to [0, gray_scale]
             scaled_row_pixels = [int(pixel * self._gray_scale / 255) for pixel in row_pixels]
             scaled_pixels.append(scaled_row_pixels)
 
-        self.processed_image = np.array(scaled_pixels, dtype=np.uint8)
+        return np.array(scaled_pixels, dtype=np.uint8)
+
+    def _process(self):
+        """
+        Overwritten method.
+        Will specify self.process_image to grayscale.
+        :return: None
+        """
+        # load raw image
+        raw_image = cv2.imread(self._image_path)
+
+        # convert to general gray level
+        gray_img = _convert2gray(raw_image)
+
+        self.processed_image = self._convert2gray_scale(gray_img)
